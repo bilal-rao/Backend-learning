f6c76d1a15912ce40d7f2754f518a7aa88938cad
Bilal Rao <bilalrao817@outlook.com>
2020-04-12 19:04:09 +0500

    implementation of fs and path core module of nodules for serving static html file to client

--- node-http/index.js
+++ node-http/index.js
@@ -1,15 +1,48 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log('req --- ', req.headers)
+    if(req.method === "GET") {
+        let fileURL;
 
+        if(req.url == "/") fileURL = '/index.html';
+        else fileURL = req.url;
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello, World</h1></body></html>')
+        let filePath = path.resolve(`./public${fileURL}`);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if(!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h2>Error 404: File Not Found!</h2></body></html>')
+                    return;
+                }
+                else {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res)
+                }
+            })
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h2>Error 404: ${fileURL} Not a html file!</h2></body></html>`)
+            return;
+        }
+    }
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h2>Error 404: ${req.method} is not supported!</h2></body></html>`)
+        return;
+    }
 })
 
 
